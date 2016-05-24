a74549ddf6cd32de3038c8dc8bd686a70c50fd2c
Michael Flowers <mikesflowers@gmail.com>
2016-05-23 21:15:40 -0500

    Made a window in PyGame!

---
+++ main.py
@@ -0,0 +1,17 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+print("Starting pygame...")
+pygame.display.init()
+screen = pygame.display.set_mode((640, 480))
+
+gameActive = True
+while gameActive:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      print("Got quit! Returning...")
+      gameActive = False
+
+print("Quitting pygame...")
+pygame.display.quit()
