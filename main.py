23d6b538b59ad2711e25cc841a27bc15f45f326b
Kelby <fubnix@hotmail.com>
2016-05-28 14:06:19 -0500

    added colored blocks

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os, sys
 import pygame
 
+WHITE=(255,255,255)
+ORANGE=(214,137,16)
 # We need more comments!
 print("Starting pygame...")
 pygame.display.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((640, 340))
 
 gameActive = True
-while gameActive:
+while gameActive: 
+
+  pygame.draw.rect(screen, WHITE, [150, 150, 200, 100], 2)
+  pygame.draw.rect(screen, ORANGE, [150, 30, 200, 100], 2)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       print("Got quit! Returning...")
       gameActive = False
+  pygame.display.flip ()     
 
 print("Quitting pygame...")
 pygame.display.quit()
